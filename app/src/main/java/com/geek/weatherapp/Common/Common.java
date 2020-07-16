64c0dc93d70a49c370c44476d44e834b83d36365
Roza Chaikozova <r.chaikozova@gmail.com>
2020-07-16 20:14:18 +0600

    first commit

---
+++ app/src/main/java/com/geek/weatherapp/Common/Common.java
@@ -0,0 +1,9 @@
+package com.geek.weatherapp.Common;
+
+import android.location.Location;
+
+public class Common {
+
+    public static final String APP_ID = "6e84f66ab53070e0f5eb3af6f09d325c";
+    public static Location current_location = null;
+}
